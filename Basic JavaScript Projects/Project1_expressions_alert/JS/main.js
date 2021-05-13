a74b519288b0def6fa1a4ce951795555815279ca
Conner Shelton <clshelt24@outlook.com>
2021-05-13 16:16:56 -0700

    Add basic HTML + JavaScript file
    as my notes say im not sure about how jumbled it seems in the browser.

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,27 @@
+//adding window alert msg.
+window.alert("Welcome to the Home page!")
+document.write(a + b);
+
+//utilizing string variable in a math problem
+var a = 4
+var b = 5
+document.write(a + b);
+
+//adding a quote using backslashes and continuations + method
+document.write("\"Be who you are and say what you feel,"                       
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr.Seuss");
+
+//adding an expresion
+document.write("Jhon"+" Doe")
+
+//button func. to display green text 
+function My_First_Function() {
+    var str="This text is green!"
+    var result=str.fontcolor("green")
+    document.getElementById("Green_Text").innerHTML=result
+};
+
+//When displaying in a browser it all seems to jumble together.
+//I've tried to fix this to the point that I assume it's a problem with how I am currently using JS.
+
