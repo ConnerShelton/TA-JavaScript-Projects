c381db45b92303e7b3e12a690af7a7234834d0d1
Conner Shelton <clshelt24@outlook.com>
2021-05-13 16:54:28 -0700

    add basic function
    text click function through JS

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+//Simple text click func.
+function Function() {
+    var sentence="I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("ClickMe").innerHTML=sentence;
+}
